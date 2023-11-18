6df31235ef85b536c4ad306adbe508fa34fc4f69
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2023-11-18 19:25:55 +0600

    fix cors

--- cmd/main.go
+++ cmd/main.go
@@ -28,14 +28,13 @@ func main() {
 	swagger.Servers = nil
 
 	router := chi.NewRouter()
-
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		MaxAge:           300,
 	})
 	router.Use(cors.Handler)
 
